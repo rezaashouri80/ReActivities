d56207bb8782443691b8a814b2afdf701dba185e
rezaashouri80 <rezaashouri8646@gmail.com>
2022-08-09 22:06:34 +0430

    create react project

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -14,6 +17,18 @@ namespace API.Controllers
             _context=context;
         }
 
+
+        [HttpGet]
+        public async Task<List<Domain.Activity>> GetAll()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Domain.Activity> GetById(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
 
     }
 }
